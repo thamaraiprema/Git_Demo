20b835ef63ad09ed0f9345b29c983b22b0847652
prema <thamaraimadhesh18@gmail.com>
2023-01-24 13:52:22 +0530

    My first project in Git using Eclipse

---
+++ src/Git_Demo/Demo_Git.java
@@ -0,0 +1,15 @@
+package Git_Demo;
+
+public class Demo_Git {
+
+	public void main(String[]args) {
+		
+	String hungry="yes";
+	if(hungry=="yes")
+		System.out.println("eat somthing...");
+	else {
+		System.out.println("Do work....");
+	}
+			
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author prema
+ *
+ */
+module Git_Demo {
+}
